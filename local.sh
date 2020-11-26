c7d26373a14cf5e29869cf6dbd4eec789c98ce80
Rod Johnson <johnsonroda@gmail.com>
2020-11-26 15:04:58 +1100

    Local testing

---
+++ local.sh
@@ -0,0 +1,6 @@
+docker build --tag springrod/dependency-cruiser .
+
+export ATOMIST_PUSH="/atm/home/commit.json"
+echo $ATOMIST_PUSH
+
+docker run  -v /Users/rod/interbeat/interbeat-app:/atm/home springrod/dependency-cruiser
--- run.sh
+++ run.sh
@@ -2,12 +2,13 @@
 
 set -o pipefail
 
-cp commit.json $ATOMIST_PUSH
-
 cd /atm/home
 
 if [[ ! -f package.json ]] ; then
     echo 'No package.json: nothing to do'
+    pwd
+    echo 'listing...'
+    ls
     exit 0
 fi
 
@@ -16,3 +17,5 @@ ls
 echo 'executing depcruise...'
 
 depcruise --include-only "^src" --output-type dot src | dot -T svg > dependencygraph.svg
+
+cp commit.json $ATOMIST_PUSH
