55456c1ccf283db9629cec5b0efb573bcef1baa9
Rod Johnson <johnsonroda@gmail.com>
2020-11-26 16:08:43 +1100

    Polishing

--- local.sh
+++ local.sh
@@ -1,7 +1,14 @@
 set -o pipefail
 
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <directory of Node project to test on>"
+  exit 1
+fi
+
 docker build --tag springrod/dependency-cruiser .
 
 export ATOMIST_OUTPUT_DIR="/atm/output"
 
-docker run  -v /Users/rod/interbeat/interbeat-app:/atm/home springrod/dependency-cruiser
+docker run  -v $1:/atm/home springrod/dependency-cruiser
+
+echo "Great success!"
