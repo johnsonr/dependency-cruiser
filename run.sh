ed311917b866f57cceaa27126a6493b7e48f9c38
Rod Johnson <johnsonroda@gmail.com>
2020-11-26 12:12:02 +1100

    Fixed?

--- run.sh
+++ run.sh
@@ -2,4 +2,10 @@
 
 set -o pipefail
 
+cp commit.json $ATOMIST_OUTPUT_DIR/push.json
+
+cd /atm/home
+
+# TODO exit 0 on not having package.json
+
 depcruise --include-only "^src" --output-type dot src | dot -T svg > dependencygraph.svg
